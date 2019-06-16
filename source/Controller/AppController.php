9a3e23a193c0bca176ebf370280126668db6658d
Vinicius Mattos <vinimattos.rj@gmail.com>
2019-06-15 21:50:00 -0300

    20190615 - Criação do controller.

--- source/Controller/AppController.php
+++ source/Controller/AppController.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vinicius
- * Date: 15/06/19
- * Time: 21:41
- */
+
+namespace App\Controller;
+
+use App\Model\User;
+
+final class AppController extends Controller
+{
+    public static function index()
+    {
+        return self::view('index');
+    }
+
+    public static function list()
+    {
+        $users = (new User)->listAll();
+        return self::view('list', ['users' => $users]);
+    }
+
+    public static function write()
+    {
+        (new User)->createNew(self::params('user'));
+        self::redirect('/list');
+    }
+
+    public static function logout()
+    {
+        (new User)->deleteAll();
+        self::redirect('/');
+    }
+
+}

9e47ba5627cc665d28854ffb0f2afef767454f65
Vinicius Mattos <vinimattos.rj@gmail.com>
2019-06-15 21:05:07 -0300

    20190615 - Criação fluxo do sistema

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use App\Router;
+$app = new Router();
+
+$app->get('/', function () {
+    return \App\Controller\AppController::index();
+});
+
+$app->get('/list', function () {
+    return \App\Controller\AppController::list();
+});
+
+$app->get('/list', function () {
+    return \App\Controller\AppController::write();
+});
+
+$app->get('/out', function () {
+    return \App\Controller\AppController::logout();
+});
+
+$app->run();
